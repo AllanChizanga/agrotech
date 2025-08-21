408535bd5c94ae60b8d1494fc72efb384da06714
allan <allanofchrist@gmail.com>
2025-08-21 13:20:22 +0200

    api

--- resources/views/exports/reports.blade.php
+++ resources/views/exports/reports.blade.php
@@ -10,26 +10,41 @@
         <table border="1" cellpadding="5" cellspacing="0">
             <thead>
                 <tr>
+                    <th>#</th>
                     <th>Question</th>
                     <th>Type</th>
-                    <th>Respondent</th>
+                    <th>Respondent Name</th>
+                    <th>National ID</th>
+                    <th>Phone</th>
+                    <th>Address</th>
+                    <th>Country</th>
+                    <th>City</th>
+                    <th>Email</th>
                     <th>Answer</th>
                     <th>Option (if any)</th>
                 </tr>
             </thead>
             <tbody>
+                @php $rowIndex = 1; @endphp
                 @foreach ($reports as $question)
                     @foreach ($question->answers as $answer)
                         <tr>
+                            <td>{{ $rowIndex++ }}</td>
                             <td>{{ $question->question_text }}</td>
                             <td>{{ $question->question_type ?? 'N/A' }}</td>
-                            <td>
-                                @if ($answer->response && $answer->response->respondent)
-                                    {{ $answer->response->respondent->fullname ?? 'N/A' }}
-                                @else
-                                    N/A
-                                @endif
-                            </td>
+                            @php
+                                $respondent =
+                                    $answer->response && $answer->response->respondent
+                                        ? $answer->response->respondent
+                                        : null;
+                            @endphp
+                            <td>{{ $respondent->fullname ?? 'N/A' }}</td>
+                            <td>{{ $respondent->national_id ?? 'N/A' }}</td>
+                            <td>{{ $respondent->phone ?? 'N/A' }}</td>
+                            <td>{{ $respondent->address ?? 'N/A' }}</td>
+                            <td>{{ $respondent->country ?? 'N/A' }}</td>
+                            <td>{{ $respondent->city ?? 'N/A' }}</td>
+                            <td>{{ $respondent->email ?? 'N/A' }}</td>
                             <td>{{ $answer->answer_text ?? 'N/A' }}</td>
                             <td>
                                 @if ($answer->option)
